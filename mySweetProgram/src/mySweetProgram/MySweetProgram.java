9c9c5939ac8c90f0f527c77b83322241f2c47f76
Isaac Roberts <isaacroberts212@gmail.com>
2020-01-26 18:33:53 -0500

    working with 2d arrays

--- mySweetProgram/src/mySweetProgram/MySweetProgram.java
+++ mySweetProgram/src/mySweetProgram/MySweetProgram.java
@@ -7,13 +7,19 @@ public class MySweetProgram {
 
 
 	public static void main(String[] args){
-
-		LinkedList<String> names = new LinkedList<String>();
-		names.push("Isaac");
-		names.push("Caleb");
-		names.push("Susan");
-		System.out.println(names.pop());
-		System.out.println(names.pop());
-		System.out.println(names.pop());
+		
+		int[][] data = {
+				{4, 6, 3, 10},
+				{4, 2, 40, 1},
+				{5, 34, 1, 43}
+		};
+		
+		
+		for(int i = 0; i < data.length; i++) {
+			for(int j = 0; j < data[i].length; j++) {
+				System.out.print(data[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
